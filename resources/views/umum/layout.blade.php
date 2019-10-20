aa96d381591f5c638dcf10b3319c0e73faf313ab
pradanna <pradanamahendra@gmail.com>
2019-10-20 15:03:13 +0700

    s

--- resources/views/umum/layout.blade.php
+++ resources/views/umum/layout.blade.php
@@ -111,14 +111,12 @@
                     <div class="row">
                         <div class="col-sm-8">
                             <img class="pt-5" src="{{asset('images/diawasiOjk.webp')}}" alt="{{asset('images/diawasiOjk.webp')}}">
+                            <img class="pt-5" src="{{asset('images/pls.webp')}}" alt="{{asset('images/pls.webp')}}">
+
                         </div>
 
                         <div class="col-sm-4">
                             <img class="pt-5" src="{{asset('images/bpr.webp')}}" alt="{{asset('images/bpr.webp')}}">
-                        </div>
-
-                        <div class="col-sm-8">
-                            <img class="pt-5" src="{{asset('images/pls.webp')}}" alt="{{asset('images/pls.webp')}}">
                         </div>
 
                     </div>
